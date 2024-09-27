20acae054e0d0368b697e6d3b51c1fa3c362d3d3
little-hash <ezraraddai166@gmail.com>
2024-09-27 12:19:35 +0300

    firstcommit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31,num2 = 2;
+const multiply = 31 * 2;
+let random 
+random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+let num3 = 8,num4 = 2
+const mod = 8/2
+let max
+max = Math.max(1,3,20,14,5)
+console.log(max)
