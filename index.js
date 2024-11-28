3f0c892cc35242ab309b7bff283b86948f71950a
Ezra <ezraraddai166@gmail.com>
2024-11-28 12:45:57 +0300

    firstcommit

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+//Multiply
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+console.log(multiply)
+
+//Random
+const random = Math.floor(Math.random() * 100) + 0
+
+//equal to 4 (mod)
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+console.log(mod)
+
+const max = Math.max(5,10,15,20);
+console.log(max)
+
